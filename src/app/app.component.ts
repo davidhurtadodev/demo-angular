cf58b382dea07056ad5aad8727393389a7ce99cf
David Hurtado <davidhuertado@gmail.com>
2024-12-26 18:35:26 +0100

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthService } from './services/auth.service';
 
@@ -18,55 +17,4 @@ export class AppComponent {
   title = 'demo-angular';
   authService = inject(AuthService);
   router = inject(Router);
-  userEmail = computed(() => this.authService.currentUser());
-
-  // logout() {
-  //   this.authService.logout();
-  // }
-
-  ngOnInit(): void {
-    // this.authService.getSession().subscribe(({ data }) => {
-    //   const isLoggedIn = !!data.session?.user;
-    //   const isLoginPage = this.router.url === '/login';
-    //   if (!isLoggedIn && !isLoginPage) {
-    //     this.router.navigateByUrl('/login');
-    //   } else if (isLoggedIn && isLoginPage) {
-    //     this.router.navigateByUrl('/');
-    //   }
-    // });
-    // const user = this.authService.currentUser();
-    // if (!user) {
-    //   console.log(this.authService.currentUser());
-    //   this.router.navigateByUrl('/login');
-    // } else {
-    //   this.router.navigateByUrl('/login');
-    // }
-    //  this.authService.currentUser.subscribe((user) => {
-    //    if (!user && this.router.url !== '/login') {
-    //      this.router.navigateByUrl('/login');
-    //    }
-    //  });
-    // this.authService.supabase.auth.onAuthStateChange((event, session) => {
-    //   if (event === 'SIGNED_IN') {
-    //     this.authService.currentUser.set({
-    //       email: session?.user.email!,
-    //     });
-    //   } else if (event === 'SIGNED_OUT') {
-    //     this.authService.currentUser.set(null);
-    //   }
-    // });
-    // this.authService.getSession().subscribe(({ data }) => {
-    //   console.log(data.session?.user.email);
-    //   if (data.session?.user.email) {
-    //     this.authService.currentUser.set({
-    //       email: data.session?.user.email!,
-    //     });
-    //   } else {
-    //     this.router.navigateByUrl('/login');
-    //   }
-    // });
-    // if (this.router.url !== '/login' && !this.authService.currentUser())
-    //   console.log(this.authService.supabase.auth.getSession());
-    // this.router.navigateByUrl('/login');
-  }
 }
